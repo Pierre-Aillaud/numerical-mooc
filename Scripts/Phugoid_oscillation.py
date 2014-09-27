656c8efd796bd001c9424bbcc4d9e1de3c55fac1
Pierre-Aillaud <pierre.aillaud.1@gmail.com>
2014-09-27 18:55:59 +0200

    Convergence
    Add lines to check the relation between convergence and time step size

--- Scripts/Phugoid_oscillation.py
+++ Scripts/Phugoid_oscillation.py
@@ -40,7 +40,29 @@ plt.ylabel('z', fontsize=14)
 plt.plot(t,z,'b-')
 plt.plot(t,z_exact,'k-')
 plt.legend(['Numerical solution', 'Analytical solution'],numpoints=1)
-
 plt.show()
 
+# time-increment array
+dt_values = numpy.array([0.1, 0.05, 0.01, 0.005, 0.001, 0.0001])
+
+# array that will contain solution of each grid
+z_values = numpy.empty_like(dt_values, dtype=numpy.ndarray)
+
+for i, dt in enumerate(dt_values):
+    N = int(T/dt)+1    # number of time-steps
+    ### discretize the time using numpy.linspace() ###
+    t = numpy.linspace(0.0, T, N)
+
+    # initial conditions
+    u = numpy.array([z0, v])
+    z = numpy.empty_like(t)
+    z[0] = z0
+    
+    # time loop - Euler method
+    for n in range(1,N):
+        ### compute next solution using Euler method ###
+        u = u + dt*numpy.array([u[1], g*(1-u[0]/zt)])
+        z[n] = u[0]   # store the elevation at time-step n+1
+    
+    z_values[i] = z.copy()    # store the total elevation calculation grid i
 
