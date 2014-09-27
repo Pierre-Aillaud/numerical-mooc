c5c441acdf70d10906ab2ba9e9914146d09aa7e6
Pierre-Aillaud <pierre.aillaud.1@gmail.com>
2014-09-27 18:17:36 +0200

    add my script

---
+++ Scripts/Phugoid_oscillation.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 23 19:16:43 2014
+
+@author: Pierre
+"""
+
+import numpy
+import matplotlib.pyplot as plt
+#Grid in time
+T = 100
+dt = 0.01
+N = int(T/dt)+1
+t = numpy.linspace(0.0,T,N)
+
+#Initial Condition
+z0 = 100.
+v = 10
+g = 9.81
+zt = 100.
+
+u = numpy.array([z0, v])
+z = numpy.zeros(N)
+z[0] = z0
+
+#time-loop using Euler's method
+for n in range(1,N):
+    u = u + dt*numpy.array([u[1],g*(1-u[0]/zt)])
+    z[n] = u[0]
+
+#Exact solution
+z_exact = v*(zt/g)**.5*numpy.sin((g/zt)**.5*t)+\
+            (z0-zt)*numpy.cos((g/zt)**.5*t)+zt  
+    
+#plot the solution
+plt.figure(figsize=(10,6))
+plt.ylim(40,160)
+plt.xlabel('t',fontsize=14)
+plt.ylabel('z', fontsize=14)
+plt.plot(t,z,'b-')
+plt.plot(t,z_exact,'k-')
+plt.legend(['Numerical solution', 'Analytical solution'],numpoints=1)
+
+plt.show()
+
+
