d5dae1513898b0ead91d05fb687ec7ca50089d06
Pierre-Aillaud <pierre.aillaud.1@gmail.com>
2014-09-27 19:26:53 +0200

    Final code
    Code before implementation of Euler function

--- Scripts/Phugoid_oscillation.py
+++ Scripts/Phugoid_oscillation.py
@@ -7,6 +7,31 @@ Created on Tue Sep 23 19:16:43 2014
 
 import numpy
 import matplotlib.pyplot as plt
+    
+def get_error(z, dt):
+    """Returns the error relative to analytical solution using L-1 norm.
+    
+    Parameters
+    ----------
+    z : array of float
+        numerical solution.
+    dt : float
+        time increment.
+        
+    Returns
+    -------
+    err : float
+        L_{1} norm of the error with respect to the exact solution.
+    """
+    N = len(z)
+    t = numpy.linspace(0.0, T, N)
+    
+    z_exact = v*(zt/g)**.5*numpy.sin((g/zt)**.5*t)+\
+                (z0-zt)*numpy.cos((g/zt)**.5*t)+zt
+    
+    return dt * numpy.sum(numpy.abs(z-z_exact))
+    
+    
 #Grid in time
 T = 100
 dt = 0.01
@@ -40,7 +65,6 @@ plt.ylabel('z', fontsize=14)
 plt.plot(t,z,'b-')
 plt.plot(t,z_exact,'k-')
 plt.legend(['Numerical solution', 'Analytical solution'],numpoints=1)
-plt.show()
 
 # time-increment array
 dt_values = numpy.array([0.1, 0.05, 0.01, 0.005, 0.001, 0.0001])
@@ -65,4 +89,21 @@ for i, dt in enumerate(dt_values):
         z[n] = u[0]   # store the elevation at time-step n+1
     
     z_values[i] = z.copy()    # store the total elevation calculation grid i
+    
+error_values = numpy.empty_like(dt_values)
+
+
+for i, dt in enumerate(dt_values):
+    ### call the function get_error() ###
+    error_values[i] = get_error(z_values[i], dt)
+    
+plt.figure(figsize=(10, 6))
+plt.tick_params(axis='both', labelsize=14) #increase tick font size
+plt.grid(True)                         #turn on grid lines
+plt.xlabel('$\Delta t$', fontsize=16)  #x label
+plt.ylabel('Error', fontsize=16)       #y label
+plt.loglog(dt_values, error_values, 'ko-')  #log-log plot
+plt.axis('equal')                      #make axes scale equally;
+    
+plt.show()
 
